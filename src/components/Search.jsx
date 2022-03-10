ee978628776b0ceef9d823bb7776d24f8159c75a
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-03-10 16:21:29 -0300

    Requisito 2 - Pair Programming

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    );
+  }
+}
+
+export default Search;
