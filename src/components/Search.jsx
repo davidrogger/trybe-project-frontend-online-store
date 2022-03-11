cc97c00a343c59aded852fcaba2524ab4d15e3fe
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-03-11 10:17:09 -0300

    PR Abrindo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ProductCard from './ProductCard';
 
-// teste PR
 class Search extends Component {
   render() {
     const {
