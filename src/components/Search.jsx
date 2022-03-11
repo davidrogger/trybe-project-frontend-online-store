c2633e174d2c3143cedd82d97d76c8cdeacd32b0
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-03-11 15:35:31 -0300

    Refatora Codigo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,43 +4,32 @@ import ProductCard from './ProductCard';
 
 class Search extends Component {
   render() {
-    const {
-      products,
-    } = this.props;
+    const { products } = this.props;
+    const searchDisplay = products.length === 0;
+    const emptySearch = <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>;
 
-    const defaultMsg = (
-      <p
-        className="search-container"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+    const productsFound = (items) => (
+      <ul>
+        {items.map(({ price, thumbnail, title, id }) => (
+          <ProductCard
+            key={ id }
+            productTitle={ title }
+            productImg={ thumbnail }
+            productPrice={ price }
+          />
+        ))}
+      </ul>
     );
-
-    if (products.length === 0) return defaultMsg;
-
     return (
-      <div
-        data-testid="home-initial-message"
-        className="search-container"
-      >
 
-        <ul
-          className="product-container"
-        >
-          {products.map(({ price, thumbnail, title, id }) => (
-            <ProductCard
-              key={ id }
-              productTitle={ title }
-              productImg={ thumbnail }
-              productPrice={ price }
-            />
-          ))}
-        </ul>
+      <div className="search-container">
+        { searchDisplay
+          ? emptySearch
+          : productsFound(products) }
       </div>
     );
   }
 }
-
 export default Search;
 
 Search.propTypes = {
