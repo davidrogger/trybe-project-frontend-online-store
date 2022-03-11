6cad8a30c16d16b949cd62416fdbdb54e817eeac
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-03-11 16:49:02 -0300

    Arrumado Req3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import InputSearch from '../Pages/InputSearch';
 import Header from './Header';
+import Cart from '../Pages/Cart';
 
 class Home extends Component {
   render() {
@@ -9,6 +10,7 @@ class Home extends Component {
       <BrowserRouter>
         <Header />
         <Switch>
+          <Route path="/cart" component={ Cart } />
           <Route exact path="/" component={ InputSearch } />
         </Switch>
       </BrowserRouter>
