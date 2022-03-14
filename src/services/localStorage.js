b0e9c0d8fab9f6847ea6d0c107d81f5e5eb78177
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-14 08:09:19 -0300

    adicionado logica de salvar review ao localstorage

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,6 +1,17 @@
-const PRODUCT_REVIEW = 'user_review';
+const PRODUCT_REVIEW_KEY = 'user_review';
 
 // Lógica reaproveitada do projeto TrybeTunes, arquivo favoriteSongsAPI.js
-if (!JSON.parse(localStorage.getItem(PRODUCT_REVIEW))) {
-  localStorage.setItem(PRODUCT_REVIEW, JSON.stringify([]));
+if (!JSON.parse(localStorage.getItem(PRODUCT_REVIEW_KEY))) {
+  localStorage.setItem(PRODUCT_REVIEW_KEY, JSON.stringify([]));
 }
+
+export const readProductReview = () => JSON.parse(localStorage
+  .getItem(PRODUCT_REVIEW_KEY));
+
+const saveProductReview = (productReview) => localStorage
+  .setItem(PRODUCT_REVIEW_KEY, JSON.stringify(productReview));
+
+export const addReview = (review) => {
+  const reviewHistoric = readProductReview();
+  saveProductReview([...reviewHistoric, review]);
+};
