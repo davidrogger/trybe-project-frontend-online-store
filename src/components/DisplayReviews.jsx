62782b1b23c51470b4e9a457206f2b1833ae3d71
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-14 08:04:30 -0300

    criado componente onde os reviews serao rendereziado usando o localStorage como base

--- src/components/DisplayReviews.jsx
+++ src/components/DisplayReviews.jsx
@@ -1,10 +1,45 @@
+// Bibliotecas
 import React, { Component } from 'react';
 
+// Serviços
+import { readProductReview } from '../services/localStorage';
+
+// Componentes
+import ReviewCard from './ReviewCard';
+
 class DisplayReviews extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviewsList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.storageReviews();
+  }
+
+  storageReviews = () => {
+    const reviewsList = readProductReview();
+    this.setState({
+      reviewsList,
+    });
+  }
+
+  // Index como key provisório, criar uma lógica de key depois.
+  allReviews = (reviewsList) => {
+    reviewsList.map((review, index) => (<ReviewCard key={ index } review={ review } />));
+  }
+
   render() {
+    const { reviewsList } = this.state;
+
     return (
-      <div>
-        <h1>ola</h1>
+      <div className="displayReview-container">
+        {reviewsList.map((review, index) => (
+          <ReviewCard key={ index } review={ review } />
+        ))}
       </div>
     );
   }
