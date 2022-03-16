5de147a7eee71d7d2e5cd2f20ed3ef130e2dbf22
Rafael Godoy <rafa.qfg@gmail.com>
2022-03-16 14:31:55 -0300

    requisito 13

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,8 +1,13 @@
 const PRODUCT_REVIEW_KEY = 'user_review';
+const CART_ITEMS_KEY = 'cart';
 
 // Lógica reaproveitada do projeto TrybeTunes, arquivo favoriteSongsAPI.js
 if (!JSON.parse(localStorage.getItem(PRODUCT_REVIEW_KEY))) {
   localStorage.setItem(PRODUCT_REVIEW_KEY, JSON.stringify([]));
+}
+
+if (!JSON.parse(localStorage.getItem(CART_ITEMS_KEY))) {
+  localStorage.setItem(CART_ITEMS_KEY, JSON.stringify([]));
 }
 
 export const readProductReview = () => JSON.parse(localStorage
@@ -14,4 +19,11 @@ const saveProductReview = (productReview) => localStorage
 export const addReview = (review) => {
   const reviewHistoric = readProductReview();
   saveProductReview([...reviewHistoric, review]);
+};
+
+export const readCartInStorage = () => JSON.parse(localStorage.getItem(CART_ITEMS_KEY));
+
+export const addCartInStorage = (cartItems) => {
+  const prevCart = readCartInStorage();
+  localStorage.setItem(CART_ITEMS_KEY, JSON.stringify([...prevCart, cartItems]));
 };
