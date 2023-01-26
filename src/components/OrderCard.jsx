d99cae5c3105d84e167ccfab579c974ef5e3f286
Davíd Roggér <davidrogger.a@gmail.com>
2023-01-26 15:47:08 -0300

    STYLE: added new style file

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 
 import ProductCartItems from './ProductCartItems';
 
+// Estilos
+import '../styles/orderCard.css';
+
 class OrderCard extends Component {
   render() {
     const { order } = this.props;
     console.log(order);
     return (
-      <div className="order-display">
+      <div className="order-display default-shadown-card ">
         <h2>{order.id}</h2>
         <ol>
           {order.products.map(({ productData, productQt }) => (
