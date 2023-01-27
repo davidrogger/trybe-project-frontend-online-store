bca3373e4251bba98cde1e15db3ff310eea72194
Davíd Roggér <davidrogger.a@gmail.com>
2023-01-27 14:43:53 -0300

    Updates

---
+++ precache-manifest.e800e7d085a6cfc5b371d62da62d8cdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b35fb359638ed06a86630c12b914976",
+    "url": "/trybe-project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "ba0c0c0ddd44ba3693cf",
+    "url": "/trybe-project-frontend-online-store/static/css/main.d7431d3d.chunk.css"
+  },
+  {
+    "revision": "65754de1c692f8942b27",
+    "url": "/trybe-project-frontend-online-store/static/js/2.bfa957e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trybe-project-frontend-online-store/static/js/2.bfa957e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0c0c0ddd44ba3693cf",
+    "url": "/trybe-project-frontend-online-store/static/js/main.40c43c6c.chunk.js"
+  },
+  {
+    "revision": "d2f04ad9a5664c8743e7",
+    "url": "/trybe-project-frontend-online-store/static/js/runtime-main.23ec2c28.js"
+  },
+  {
+    "revision": "a24a6cd0ac43ae31dec662eaed5b464d",
+    "url": "/trybe-project-frontend-online-store/static/media/carshop2.a24a6cd0.png"
+  },
+  {
+    "revision": "4bab11ab7bacaae7d6f47203407cafd4",
+    "url": "/trybe-project-frontend-online-store/static/media/search2.4bab11ab.png"
+  }
+]);
